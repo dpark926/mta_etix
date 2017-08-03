4a323ae9e75fb17a2da4b66f8588f6243ef2b83b
Daniel Park <dpeace926@gmail.com>
2017-08-03 03:49:01 -0400

    added timedate component

--- colorful-timedate/src/App.js
+++ colorful-timedate/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timedate from './components/Timedate.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/* <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <Timedate/>
       </div>
     );
   }
