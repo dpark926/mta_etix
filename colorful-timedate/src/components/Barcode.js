e2743da66bf2c8b2c2086aed0b7d17512365d05f
Daniel Park <dpeace926@gmail.com>
2017-08-08 20:48:42 -0400

    added Barcode component w/img

---
+++ colorful-timedate/src/components/Barcode.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Barcode () {
+  return (
+    <div>
+      <div className='barcode-main'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Barcode
--- colorful-timedate/src/containers/AppContainer.js
+++ colorful-timedate/src/containers/AppContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import TimeContainer from './TimeContainer.js'
 import ColorBlock from '../components/ColorBlock.js'
 import Button from '../components/Button.js'
+import Barcode from '../components/Barcode.js'
+import barcodeImg from '../styles/imgs/2d-barcode.png'
 
 class AppContainer extends Component {
   constructor() {
@@ -16,21 +18,31 @@ class AppContainer extends Component {
 
   handleClick() {
     this.setState({
-      clicked: true
+      clicked: !this.state.clicked
     })
   }
 
   render() {
-    return (
-      <div className="app-container">
+    if (this.state.clicked === false) {
+      return (
+        <div className="app-container">
 
-        <div className='time-block-wrapper'>
-          <TimeContainer handleClick={this.handleClick}/>
-          <ColorBlock handleClick={this.handleClick}/>
+          <div className='time-block-wrapper'>
+            <TimeContainer handleClick={this.handleClick}/>
+            <ColorBlock handleClick={this.handleClick}/>
+          </div>
+          <a href='#'><Button handleClick={this.handleClick}/></a>
         </div>
-        <Button handleClick={this.handleClick}/>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="app-container">
+          <Barcode/>
+          <img src={barcodeImg}></img>
+          <a href='#'><Button handleClick={this.handleClick}/></a>
+        </div>
+      );
+    }
   }
 }
 
