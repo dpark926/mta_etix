2f80fff5abdd4778b6141258e63dfa535a76b505
Daniel Park <dpeace926@gmail.com>
2017-08-09 23:47:27 -0400

    added Origin & Destination components

---
+++ colorful-timedate/src/components/Destination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Destination () {
+  return (
+    <div>
+      <input placeholder="Search for Destinations"></input>
+    </div>
+  )
+}
+
+export default Destination
---
+++ colorful-timedate/src/components/Origin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Origin () {
+  return (
+    <div>
+      <input placeholder="Search for Origins"></input>
+    </div>
+  )
+}
+
+export default Origin
