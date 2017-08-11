c7090a2a520a41a96b51336baf17a7a83016ccce
Daniel Park <dpeace926@gmail.com>
2017-08-10 22:40:11 -0400

    updated Origin and Destination Components with locations in listed

--- colorful-timedate/src/components/Destination.js
+++ colorful-timedate/src/components/Destination.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../styles/OriginDest.css'
+
+let locations = ['Albertson', 'Amagansett', 'Amityville', 'Ansonia', 'Appalachian Trail',
+  'Ardsley on Hudson', 'Atlantic Terminal', 'Auburndale', 'Babylon', 'Baldwin',
+  'Bay Shore', 'Bayside', 'Beacon', 'Beacon-Falls', 'Beadford Hills',
+  'Bellrose', 'Bellmore', 'Bellport', 'Bethel', 'Bethpage',
+  'Botanical Garden', 'Branchville', 'Breakneck Ridge', 'Brentwood', 'Brewster',
+  'Bridgehampton', 'Bridgeport', 'Broadway', 'Bronxville', 'Cannondale']
+
+let loc = locations.map( location => <div className="location-list-blah"><a href="#"><div className="location-each">{location}</div></a></div>)
 
 function Destination () {
   return (
     <div>
-      <input placeholder="Search for Destinations"></input>
+      <div className="location-header">
+        <h2 className="location-header-text">Select Origin Station</h2>
+      </div>
+      <input className="location-search"placeholder="Search for Destinations"></input>
+      <div className="location-list">
+        {loc}
+      </div>
     </div>
   )
 }
--- colorful-timedate/src/containers/AppContainer.js
+++ colorful-timedate/src/containers/AppContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Origin from '../components/Origin.js'
+import Destination from '../components/Destination.js'
 import TimeContainer from './TimeContainer.js'
 import ColorBlock from '../components/ColorBlock.js'
 import Button from '../components/Button.js'
@@ -11,6 +13,8 @@ class AppContainer extends Component {
     this.state = {
       clicked: false,
       activated: (new Date()).toLocaleTimeString(),
+      origin: '',
+      destination: '',
     }
 
     this.handleClick = this.handleClick.bind(this)
@@ -23,37 +27,45 @@ class AppContainer extends Component {
   }
 
   render() {
-    if (this.state.clicked === false) {
-      return (
-        <div className="app-container">
-          <div className='time-block-button-wrapper animated slideInDown'>
-            <div className='time-block-wrapper'>
-              <TimeContainer handleClick={this.handleClick}/>
-              <ColorBlock handleClick={this.handleClick}/>
-              <p>Tap to reveal barcode</p>
-            </div>
-            <a href='#'><Button handleClick={this.handleClick} clicked={this.state.clicked}/></a>
-          </div>
-          <p>This is a message</p>
-          <p>Activated at: {this.state.activated}</p>
-        </div>
-      );
-    } else {
-      return (
-        <div className="app-container">
-          <div className='time-block-button-wrapper animated slideInUp'>
-            <div className='time-block-wrapper'>
-              <Barcode/>
-              <p>Tap to view color bar</p>
-            </div>
-            <a href='#'><Button handleClick={this.handleClick} clicked={this.state.clicked}/></a>
-          </div>
-          <p>This is a message</p>
-          <p>Activated at: {this.state.activated}</p>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <Origin/>
+        <Destination/>
+      </div>
+    )
   }
+  // render() {
+  //   if (this.state.clicked === false) {
+  //     return (
+  //       <div className="app-container">
+  //         <div className='time-block-button-wrapper animated slideInDown'>
+  //           <div className='time-block-wrapper'>
+  //             <TimeContainer handleClick={this.handleClick}/>
+  //             <ColorBlock handleClick={this.handleClick}/>
+  //             <p>Tap to reveal barcode</p>
+  //           </div>
+  //           <a href='#'><Button handleClick={this.handleClick} clicked={this.state.clicked}/></a>
+  //         </div>
+  //         <p>This is a message</p>
+  //         <p>Activated at: {this.state.activated}</p>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className="app-container">
+  //         <div className='time-block-button-wrapper animated slideInUp'>
+  //           <div className='time-block-wrapper'>
+  //             <Barcode/>
+  //             <p>Tap to view color bar</p>
+  //           </div>
+  //           <a href='#'><Button handleClick={this.handleClick} clicked={this.state.clicked}/></a>
+  //         </div>
+  //         <p>This is a message</p>
+  //         <p>Activated at: {this.state.activated}</p>
+  //       </div>
+  //     );
+  //   }
+  // }
 }
 
 // Link?
