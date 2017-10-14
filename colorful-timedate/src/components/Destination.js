508120990a6147248fdcd5b6188c5741c5e2c903
Daniel Park <dpeace926@gmail.com>
2017-10-14 00:49:51 -0400

    extend Origin/Destinations and add letters to separate

--- colorful-timedate/src/components/Destination.js
+++ colorful-timedate/src/components/Destination.js
@@ -2,12 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/OriginDest.css'
 
-let locations = ['Albertson', 'Amagansett', 'Amityville', 'Ansonia', 'Appalachian Trail',
-  'Ardsley on Hudson', 'Atlantic Terminal', 'Auburndale', 'Babylon', 'Baldwin',
+let locations = ['A', 'Albertson', 'Amagansett', 'Amityville', 'Ansonia', 'Appalachian Trail',
+  'Ardsley on Hudson', 'Atlantic Terminal', 'Auburndale', 'B', 'Babylon', 'Baldwin',
   'Bay Shore', 'Bayside', 'Beacon', 'Beacon-Falls', 'Beadford Hills',
   'Bellrose', 'Bellmore', 'Bellport', 'Bethel', 'Bethpage',
   'Botanical Garden', 'Branchville', 'Breakneck Ridge', 'Brentwood', 'Brewster',
-  'Bridgehampton', 'Bridgeport', 'Broadway', 'Bronxville', 'Cannondale']
+  'Bridgehampton', 'Bridgeport', 'Broadway', 'Bronxville', 'C', 'Cannondale', 'Carle Place',
+  'Cedarhurst', 'Central Islip', 'Centre Avenue', 'Chappaqua', 'Cold Spring',
+  'Cold Spring Harbor', 'Copiague', 'Cortlandt', 'Cos Cob', 'Country Life Press',
+  'Crestwood', 'Croton Falls', 'Corton-Harmon', 'D', 'Danbury', 'Darien', 'Deer Park',
+  'Derby-Shelton', 'Dobbs Ferry', 'Douglaston', 'Dover Plains', 'E', 'East Hampton',
+  'East New York', 'East Norwalk', 'East Rockaway', 'East Wiliston', 'F', 'Fairfield',
+  'Fairfield Metro', 'Far Rockaway', 'Farmingdale', 'Fleetwood', 'Floral Park',
+  'Flushing Main Street', 'Fordham', 'Forest Hills', 'Freeport', 'Garden City',
+  'G', 'Garrison', 'Gibson', 'Glen Cove', 'Glen Head', 'Geln Street', 'Glenbrook',
+  'Glenwood', 'Goldens Bridge', 'Great Neck', 'Great River']
 
 function Destination (props) {
   let destination = props.destination
@@ -22,13 +31,25 @@ function Destination (props) {
     }
   }
 
-  let loc = newArray.map( location =>
-    <div className="location-list-blah">
-      <Link to="/ticket-type">
-        <div className="location-each" id={location}>{location}</div>
-      </Link>
-    </div>
-  )
+  let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  let loc = newArray.map( location => {
+    if (alpha.includes(location)) {
+      return (
+        <div className='alpha'>
+          <div className='letter'>{location}</div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="location-list-blah">
+          <Link to="/ticket-type">
+            <div className="location-each" id={location}>{location}</div>
+          </Link>
+        </div>
+      )
+    }
+  })
 
   return (
     <div>
@@ -43,6 +64,16 @@ function Destination (props) {
           <div className="origin-destination-third"></div>
         </div>
         <div className="location-list">
+          <div className="location-list-blah">
+            <Link to="/ticket-type">
+              <div className="location-each" id='Grand Central Station'>Grand Central Station</div>
+            </Link>
+          </div>
+          <div className="location-list-blah">
+            <Link to="/ticket-type">
+              <div className="location-each" id='Penn Station'>Penn Station</div>
+            </Link>
+          </div>
           {loc}
         </div>
       </div>
