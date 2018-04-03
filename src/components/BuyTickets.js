fe856989ae4b07212f6b322228d666a8dbe0d6cb
Daniel Park <dpeace926@gmail.com>
2018-04-02 23:49:07 -0400

    style buy ticket component

--- src/components/BuyTickets.js
+++ src/components/BuyTickets.js
@@ -10,28 +10,27 @@ function BuyTickets () {
         <div className="location-header nav justify-content-center">
           <div className="location-header-bars fa fa-bars" style={{fontSize: "1.5em"}}></div>
           <h2 className="location-header-text">MTA eTix</h2>
-
         </div>
-        <div className="animated slideInRight">
-          <Link to='/origin'>
-            <div className="button">
-              <div className="buy-ticket-text">Buy Tickets</div>
-              <div className="buy-ticket-arrow fa fa-arrow-right"></div>
-            </div>
-          </Link>
-          <Link to='/wallet'>
-            <div className="button">
-              <div className="buy-ticket-text">Ticket Wallet</div>
-              <div className="buy-ticket-arrow fa fa-arrow-right"></div>
-            </div>
-          </Link>
-          {/* <Link to='/origin'> */}
-            <div className="button">
-              <div className="buy-ticket-text">Train Time</div>
-              <div className="buy-ticket-arrow fa fa-arrow-right"></div>
-            </div>
-          {/* </Link> */}
-        </div>
+      </div>
+      <div className="buy-tickets-wrapper">
+        <Link to='/origin'>
+          <div className="button">
+            <div className="buy-ticket-text">Buy Tickets</div>
+            <div className="buy-ticket-arrow fa fa-arrow-right"></div>
+          </div>
+        </Link>
+        <Link to='/wallet'>
+          <div className="button">
+            <div className="buy-ticket-text">Ticket Wallet</div>
+            <div className="buy-ticket-arrow fa fa-arrow-right"></div>
+          </div>
+        </Link>
+        {/* <Link to='/origin'> */}
+          <div className="button">
+            <div className="buy-ticket-text">Train Time</div>
+            <div className="buy-ticket-arrow fa fa-arrow-right"></div>
+          </div>
+        {/* </Link> */}
       </div>
     </div>
 
