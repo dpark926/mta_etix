1a05f6aa7a3408d4c9a4cf25c529089c8400606a
Daniel Park <dpeace926@gmail.com>
2019-04-29 22:11:18 -0400

    delete dup, tweak

--- src/js/containers/TimeContainer.js
+++ src/js/containers/TimeContainer.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import '../../styles/TimeDate.css';
+import React, { Component } from "react";
+import "../../styles/TimeDate.css";
 
 class TimeContainer extends Component {
-
   constructor() {
-    super()
+    super();
 
-    let d = new Date()
+    let d = new Date();
     this.state = {
       day: d.getDay(),
       month: d.getMonth(),
@@ -15,14 +14,14 @@ class TimeContainer extends Component {
       hour: d.getHours(),
       minute: d.getMinutes(),
       second: d.getSeconds(),
-      time: d.toLocaleTimeString(),
-    }
+      time: d.toLocaleTimeString()
+    };
 
-    this.countingSecond = this.countingSecond.bind(this)
+    this.countingSecond = this.countingSecond.bind(this);
   }
 
   countingSecond() {
-    let d = new Date()
+    let d = new Date();
     this.setState({
       day: d.getDay(),
       month: d.getMonth(),
@@ -33,29 +32,51 @@ class TimeContainer extends Component {
       second: d.getSeconds(),
       milliseconds: d.getMilliseconds(),
       time: d.toLocaleTimeString()
-    })
+    });
   }
 
   componentWillMount() {
-    setInterval(this.countingSecond, 1000)
+    setInterval(this.countingSecond, 1000);
   }
 
   render() {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "November",
+      "December"
+    ];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
     const { time, month, date, year } = this.state;
 
     return (
-      <div className='timeclock-main'>
-        <div className='timeclock-text'>
-          <div className='timeclock-time'>{time}</div>
-          <div className='timeclock-date'>
-            {month >= 10 ? month + 1 : '0' + month}/{date < 10 ? '0' + date : date}/{year}
+      <div className="timeclock-main">
+        <div className="timeclock-text">
+          <div className="timeclock-time">{time}</div>
+          <div className="timeclock-date">
+            {month >= 10 ? month + 1 : "0" + (month + 1)}/{date < 10
+              ? "0" + date
+              : date}/{year}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default TimeContainer
+export default TimeContainer;
